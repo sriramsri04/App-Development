f4962b9691c0cd1bd1de77e0291aec4650fe6b54
sriramsri04 <120361531+sriramsri04@users.noreply.github.com>
2023-10-05 15:03:09 +0530

    Add files via upload

---
+++ SB-Day-13/ClothHub_Store/src/main/java/com/app/entity/Category.java
@@ -0,0 +1,77 @@
+package com.app.entity;
+
+import com.app.entity.Product;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+
+	@OneToMany(mappedBy = "category")
+	private List<Product> products;
+
+	// Constructors
+
+	public Category() {
+		// Default constructor
+	}
+
+	public Category(String name) {
+		this.name = name;
+	}
+
+	// Getters and Setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
+//    // Additional methods if needed
+//
+//    public void addProduct(Product product) {
+//        products.add(product);
+//        product.setCategory(this);
+//    }
+//
+//    public void removeProduct(Product product) {
+//        products.remove(product);
+//        product.setCategory(null);
+//    }
+
+	// Overrides
+
+	@Override
+	public String toString() {
+		return "Category{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				'}';
+	}
+}
